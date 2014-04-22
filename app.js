047cf11209771964cc4c11b16b3030a99395fd64
Max Gooding <maxgooding@gmail.com>
2014-04-22 16:14:10 -0600

    Added names
    RANDOM NAMES!

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var app = require('express')()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server)
-  , UUID = require('node-uuid');
+  , UUID = require('node-uuid')
+  , arrayfile = require('./public/arrays');
 
 var express = require('express');
 app.use('/public', express.static(__dirname+'/public'));
@@ -16,6 +17,10 @@ app.get('/', function (req, res) {
 var currentPlayers = [];
 var availColors = ["blue", "red", "green"];
 
+// random names
+var names = arrayfile.names;
+var adjectives = arrayfile.adjectives;
+
 var connectedUsers = [];
 
 var gameBoard = new Array(800);
@@ -29,25 +34,36 @@ for (var i=0; i<gameBoard.length; i++){
     }
 }
 
-function Player(x, y, clientID){
+function User (clientID, nickname){
+    this.clientID = clientID;
+    this.nickname = nickname;
+}
+
+function Player(x, y, clientID, nickname){
     this.color = availColors.pop();
     this.x = x;
     this.y = y;
     this.clientID = clientID;
     this.direction = 'up';
     this.alive = true;
+    this.nickname = nickname;
 }
 
 setInterval(gameLoop, 25);
 function gameLoop(){
             if (currentPlayers.length == 0 && connectedUsers.length >= 2){
-                for (var i=0; i < 2; i++){
+                for (var i=0; i < 3; i++){
                     switch (i) {
                         case 0:
-                            currentPlayers[0] = new Player(300, 250, connectedUsers[i]);
+                            currentPlayers[0] = new Player(300, 250, connectedUsers[i].clientID, connectedUsers[i].nickname);
                             break;
                         case 1:
-                            currentPlayers[1] = new Player(500, 250, connectedUsers[i]);
+                            currentPlayers[1] = new Player(500, 250, connectedUsers[i].clientID, connectedUsers[i].nickname);
+                            break;
+                        case 2:
+                            if (connectedUsers.length > 2){
+                                currentPlayers[2] = new Player(400, 250, connectedUsers[i].clientID, connectedUsers[i].nickname);
+                            }
                             break;
                     }
                 }
@@ -91,44 +107,6 @@ function movePlayer(p) {
      }
 }
 
-function movePlayer2(p) {
-    var w = 800, h = 500;
-    var ph = 10;
-    var ps = 10;
-    var mx = 0, my = 0;
-    var died = false;
-    switch (p.direction) {
-        case 'up':
-            my = -1;
-            break;
-        case 'down':
-            my = 1;
-            break;
-        case 'left':
-            mx = -1;
-            break;
-        case 'right':
-            mx = 1;
-            break;
-    }
-    var dy = Math.max(Math.min(w-ph, p.y+my*ps), 0);
-    var dx = Math.max(Math.min(h-ph, p.x+mx*ps), 0);
-    if (dy !== p.y+my*ps || dx !== p.x+mx*ps) { died = true; }
-    for (var i = p.x; i < p.x + dx + ph*Math.abs(my); i++){
-        for (var j = p.y; j < p.y + dy + ph*Math.abs(mx); j++){
-            if (gameBoard[i][j] !== false){ 
-                died = true;
-            } else {
-                gameBoard[i][j] = true;
-            }
-        }
-    }
-    p.y += dy;
-    p.x += dx;
-    
-}
-
-
 io.sockets.on('connection', function (client) {
         //Generate a new UUID, looks something like 
         //5b2ca132-64bd-4513-99da-90e838ca47d1
@@ -137,7 +115,12 @@ io.sockets.on('connection', function (client) {
 
         //Useful to know when someoce connects
         console.log('\t socket.io:: player ' + client.userid + ' connected');
-        connectedUsers.push(client.userid);
+        var nameIndex = Math.floor(Math.random()*names.length);
+        var adjIndex = Math.floor(Math.random()*adjectives.length);
+        var randName = adjectives[adjIndex] + " " + names[nameIndex];
+        randName = randName.charAt(0).toUpperCase() + randName.slice(1);
+        var newUser = new User(client.userid, randName);
+        connectedUsers.push(newUser);
 
         //When this client changes direction
         client.on('changedirection', function (data) {
@@ -161,7 +144,7 @@ io.sockets.on('connection', function (client) {
             }
 
             for (var i=0; i<connectedUsers.length;i++){
-                if (connectedUsers[i] == client.userid){
+                if (connectedUsers[i].clientID == client.userid){
                     connectedUsers.splice(i, 1);
                 }
             }
